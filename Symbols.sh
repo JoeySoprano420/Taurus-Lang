5695f31bcb231531a586bd4b73c90653e4f7872b
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-05-21 16:22:09 -0400

    Create Symbols.sh

---
+++ Symbols.sh
@@ -0,0 +1,12 @@
++ - * / %        → arithmetic
+== != < > <= >=  → comparisons
+&& || !          → logical
+^                → exponent
+~>               → raise
+->               → transition/progress
+<< >>            → rollback/run
+[]               → buffer
+()               → texturizer (e.g. function calls)
+{}               → block
+#                → single-line comment
+** ... **        → multi-line comment
